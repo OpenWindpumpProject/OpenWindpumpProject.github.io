0b6b00c87b527a4c0ee6062ab273575a8bf56694
k4mi <b.sennholz@web.de>
2014-12-23 14:40:55 +0100

    Issue #2: Gallery rework
    - created own subfolder for gallery
    - reworked image thumbs (for maximum size in mobile view)
    - added hover placeholder text
    - view Issue #9

--- js/mailto.js
+++ js/mailto.js
@@ -5,10 +5,10 @@
 function mailTo() {
     var link = "mailto:m.hammer@green-desert.info"
              //+ "?cc=myCCaddress@example.com"
-             + "&subject=" + escape("OWP Contact Form: " + document.getElementById('contact_subject').value)
-             + "&body=Name: " + escape(document.getElementById('contact_name').value)
+             + "?subject=" + encodeURI("OWP Contact Form: " + document.getElementById('contact_subject').value)
+             + "&body=Name: " + encodeURI(document.getElementById('contact_name').value)
              //+ escape("\nE-Mail: " + document.getElementById('contact_email').value)
-             + escape("\nMessage: " + document.getElementById('contact_message').value)
+             + encodeURI("\nMessage: " + document.getElementById('contact_message').value)
     ;
  
     window.location.href = link;
